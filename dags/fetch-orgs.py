564ae36b88d10620a1c6095bb5ee742cc352c6ea
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-07-15 22:02:49 +0200

    wip

--- dags/fetch-orgs.py
+++ dags/fetch-orgs.py
@@ -15,43 +15,48 @@ from airflow.providers.trino.hooks.trino import TrinoHook
     dagrun_timeout=datetime.timedelta(minutes=3),
 )
 def ProcessGithubOrgs():
-    create_github_schema = TrinoOperator(
-        task_id="create_github_schema",
-        sql="""
-            CREATE SCHEMA IF NOT EXISTS lakehouse.github WITH (
-                location = 's3a://open-source-dashboards/github'
-            )""",
-    )
+    @task()
+    def create_github_schema():
+        TrinoOperator(
+            task_id="create_github_schema",
+            sql="""
+                CREATE SCHEMA IF NOT EXISTS lakehouse.github WITH (
+                    location = 's3a://open-source-dashboards/github'
+                )""",
+        )
 
-    create_github_orgs_table = TrinoOperator(
-        task_id="create_github_orgs_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS lakehouse.github.orgs (
-                id bigint,
-                node_id varchar,
-                login varchar,
-                url varchar,
-                repos_url varchar,
-                events_url varchar,
-                hooks_url varchar,
-                issues_url varchar,
-                members_url varchar,
-                public_members_url varchar,
-                avatar_url varchar,
-                description varchar,
-                load_ts timestamp(6)
-            ) WITH (
-                format = 'PARQUET'
-            )""",
-    )
+    @task()
+    def create_github_orgs_table():
+        TrinoOperator(
+            task_id="create_github_orgs_table",
+            sql="""
+                CREATE TABLE IF NOT EXISTS lakehouse.github.orgs (
+                    id bigint,
+                    node_id varchar,
+                    login varchar,
+                    url varchar,
+                    repos_url varchar,
+                    events_url varchar,
+                    hooks_url varchar,
+                    issues_url varchar,
+                    members_url varchar,
+                    public_members_url varchar,
+                    avatar_url varchar,
+                    description varchar,
+                    load_ts timestamp(6)
+                ) WITH (
+                    format = 'PARQUET'
+                )""",
+        )
 
     @task()
     def get_max_org_id():
         result = TrinoHook().get_records("SELECT max(id) FROM lakehouse.github.orgs")
         return result[0][0]
 
-    create_github_schema >> create_github_orgs_table >> get_max_org_id()
+
+    create_github_schema()
+    create_github_orgs_table()
     max_org_id = get_max_org_id()
-    print(max_org_id)
 
 dag = ProcessGithubOrgs()
